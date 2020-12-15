7705b5810788623fd59c1a21c169361c9e145d0e
Curtis Bartell <cbartell980@gmail.com>
2020-12-15 15:57:23 -0700

    Add Item relationship tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe 'Item' do
   describe 'relationships' do
-
+    it { should belong_to(:merchant) }
+    it { should have_many(:invoice_items) }
+    it { should have_many(:invoices).through(:invoice_items) }
   end
 
   describe 'validations' do
-    
+
   end
 end
