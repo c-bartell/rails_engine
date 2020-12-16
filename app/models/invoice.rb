a343c702c3ea3a7f35a99c4f832289cdcda07d5c
Curtis Bartell <cbartell980@gmail.com>
2020-12-15 20:06:09 -0700

    Add attribute validations to Invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,6 @@ class Invoice < ApplicationRecord
  has_many :invoice_items
  has_many :items, through: :invoice_items
  belongs_to :merchant
+
+ validates_presence_of :status, :customer_id, :merchant_id
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:items).through(:invoice_items) }
     it { should belong_to(:merchant) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:customer_id) }
+    it { should validate_presence_of(:merchant_id) }
+  end
 end
