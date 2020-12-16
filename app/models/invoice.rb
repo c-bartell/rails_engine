d4e5d19c90f75f94b52c24a31468dc3fcefbfdd4
Curtis Bartell <cbartell980@gmail.com>
2020-12-15 20:28:42 -0700

    Add customer relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ApplicationRecord
  has_many :invoice_items
  has_many :items, through: :invoice_items
  belongs_to :merchant
+ belongs_to :customer
 
  validates_presence_of :status, :customer_id, :merchant_id
 end
