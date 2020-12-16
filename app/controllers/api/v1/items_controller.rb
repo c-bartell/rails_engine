a46f704c174c07ba20719c1378ad35dca96dc5ac
Curtis Bartell <cbartell980@gmail.com>
2020-12-16 14:31:15 -0700

    Add Item create endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.format_item(Item.find(params[:id]))
   end
+
+  def create
+    render json: ItemSerializer.format_item(Item.create(item_params))
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
