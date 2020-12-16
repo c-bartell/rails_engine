7e8caee3d0ba3002e0cc1848c8ebaec8d60c9893
Curtis Bartell <cbartell980@gmail.com>
2020-12-15 17:11:43 -0700

    Add attribute validations for Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  validates_presence_of :name, :description, :unit_price, :merchant_id
+  validates :unit_price, numericality: { greater_than_or_equal_to: 0 }
 end
