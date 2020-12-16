1da23f18ee549598eb11a17b17a9212c8f183213
Curtis Bartell <cbartell980@gmail.com>
2020-12-15 19:53:35 -0700

    Add quantity and unit_price validations to InvoiceItem

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,5 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
 
   validates_presence_of :item_id, :invoice_id
+  validates :quantity, :unit_price, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -9,5 +9,7 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:item_id) }
     it { should validate_presence_of(:invoice_id) }
+    it { should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }
   end
 end
