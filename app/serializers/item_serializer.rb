db1c8f7e337d9abe057264ad46ea16c357c86af7
Curtis Bartell <cbartell980@gmail.com>
2020-12-16 14:05:31 -0700

    Add item show endpoint

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,18 +1,36 @@
 class ItemSerializer
   def self.format_item(items)
     {
-      data: items.map do |item|
-        {
-        id: item.id,
-        type: 'item',
-        attributes: {
-            merchant_id: item.merchant_id,
-            name: item.name,
-            description: item.description,
-            unit_price: item.unit_price
-          }
-        }
+      data: self.formatted_data(items)
+    }
+  end
+
+  private
+
+  def self.formatted_data(item_data)
+    if item_data.is_a? Item
+      self.item_hash(item_data)
+    else
+      item_data.map do |item|
+        self.item_hash(item)
       end
+    end
+  end
+
+  def self.item_hash(item)
+    {
+      id: item.id.to_s,
+      type: 'item',
+      attributes: self.attributes_hash(item)
+    }
+  end
+
+  def self.attributes_hash(item)
+    {
+      merchant_id: item.merchant_id.to_s,
+      name: item.name,
+      description: item.description,
+      unit_price: item.unit_price
     }
   end
 end
