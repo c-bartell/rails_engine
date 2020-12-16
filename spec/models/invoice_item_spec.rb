2fc80ecfb748719a8ae8c0dc77159c798f63be8d
Curtis Bartell <cbartell980@gmail.com>
2020-12-15 19:58:33 -0700

    Add presence validators for ItemOrder quantity and unit_price

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:item_id) }
     it { should validate_presence_of(:invoice_id) }
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
     it { should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
     it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }
   end
