134453e8ad03a9c886db9f0da9287723bdcdcd23
Curtis Bartell <cbartell980@gmail.com>
2020-12-16 14:30:58 -0700

    Add tests for Item create endpoint

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -75,4 +75,25 @@ RSpec.describe 'Items ReST Endpoints' do
     expect(item[:attributes]).to have_key(:unit_price)
     expect(item[:attributes][:unit_price]).to be_an(Float)
   end
+
+  it 'can create a new item' do
+    merchant = create(:merchant)
+    item_params = {
+      name: 'Kajigger',
+      description: 'A whachacallit.',
+      unit_price: 0.99,
+      merchant_id: create(:merchant).id
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end
