98fffa41186f19593ef951849499141c3852584f
Curtis Bartell <cbartell980@gmail.com>
2020-12-16 19:40:28 -0700

    Add tests for Merchant show endpoint

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'Merchant ReST endpoints' do
 
     expect(response).to be_successful
 
-    merchant_response = JSON.parse(response.body, symbolize_names: true)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchant_response).to have_key(:data)
-    expect(merchant_response[:data]).to be_an(Array)
-    expect(merchant_response[:data].count).to eq(20)
+    expect(parsed_response).to have_key(:data)
+    expect(parsed_response[:data]).to be_an(Array)
+    expect(parsed_response[:data].count).to eq(20)
 
-    merchants = merchant_response[:data]
+    merchants = parsed_response[:data]
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
@@ -29,5 +29,32 @@ RSpec.describe 'Merchant ReST endpoints' do
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_an(String)
     end
+  end
+
+  it 'can return a single merchant from an id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response).to have_key(:data)
+    expect(parsed_response[:data]).to be_an(Hash)
+
+    merchant = parsed_response[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to eq('merchant')
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes]).to be_an(Hash)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
   end
 end
