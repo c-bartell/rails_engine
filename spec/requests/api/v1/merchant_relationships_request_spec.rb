2c4bb9105a2875b93727c4f1bb7cd1747bd12d44
Curtis Bartell <cbartell980@gmail.com>
2020-12-17 13:41:16 -0700

    Add tests for merchant items endpoint

--- spec/requests/api/v1/merchant_relationships_request_spec.rb
+++ spec/requests/api/v1/merchant_relationships_request_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant relationship requests' do
   it 'can return all items associated with a merchant' do
-    create(:merchant).items << create_list(:item, 5)
-    id = Merchant.last.id
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
 
-    get "/api/v1/merchants/#{id}/items"
+    3.times do
+      merchant_1.items << create(:item)
+    end
+
+    5.times do
+      merchant_2.items << create(:item)
+    end
+
+    get "/api/v1/merchants/#{merchant_1.id}/items"
 
     expect(response).to be_successful
 
-    parsed_response = JSON.parse(response.body, sympolize_names: true)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
 
     expect(parsed_response).to have_key(:data)
     expect(parsed_response[:data]).to be_an(Array)
 
     items = parsed_response[:data]
 
-    expect(items.count).to eq(5)
+    expect(items.count).to eq(3)
 
     items.each do |item|
       expect(item).to have_key(:id)
@@ -30,6 +38,7 @@ RSpec.describe 'Merchant relationship requests' do
 
       expect(item[:attributes]).to have_key(:merchant_id)
       expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:attributes][:merchant_id]).to eq(merchant_1.id)
 
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to be_an(String)
