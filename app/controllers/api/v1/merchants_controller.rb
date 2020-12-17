0d2cd6d7496a779c690e95198a1d468d7581024d
Curtis Bartell <cbartell980@gmail.com>
2020-12-16 19:51:35 -0700

    Add Merchant create endpoint

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.format_merchant(Merchant.find(params[:id]))
   end
+
+  def create
+    render json: MerchantSerializer.format_merchant(Merchant.create(merchant_params))
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
