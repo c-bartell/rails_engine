421d6befe1461913ad8b7393a181e23f420fd44a
Curtis Bartell <cbartell980@gmail.com>
2020-12-16 19:40:56 -0700

    Add Merchant show endpoint

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,10 +1,21 @@
 class MerchantSerializer
   def self.format_merchant(merchants)
     {
-      data: merchants.map do |merchant|
+      data: # merchants.map do |merchant|
+      #   merchant_hash(merchant)
+      # end
+      formatted_data(merchants)
+    }
+  end
+
+  def self.formatted_data(merchant_data)
+    if merchant_data.is_a? Merchant
+      merchant_hash(merchant_data)
+    else
+      merchant_data.map do |merchant|
         merchant_hash(merchant)
       end
-    }
+    end
   end
 
   def self.merchant_hash(merchant)
