56ad86a129fc1de9ff4c2096969696688439c789
Curtis Bartell <cbartell980@gmail.com>
2020-12-17 18:46:29 -0700

    Add tests for Merchant::search

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'class methods' do
+    describe '::search' do
+      it 'name' do
+        create_list :merchant, 9
+        merchant_params = {
+          name: 'The Arasaka Corporation'
+        }
+        target_id = Merchant.create(merchant_params).id
+
+        name_fragment = {
+          name: 'e Ara'
+        }
+        different_case = {
+          name: 'SAKA cORp'
+        }
+        something_that_isnt_there = {
+          name: 'The Guild of Calamitous Intent'
+        }
+        empty_query = {}
+        expect(Merchant.search(merchant_params).id).to eq(target_id)
+        expect(Merchant.search(name_fragment).id).to eq(target_id)
+        expect(Merchant.search(different_case).id).to eq(target_id)
+        expect(Merchant.search(something_that_isnt_there)).to be_nil
+        expect(Merchant.search(empty_query)).to be_nil
+      end
+    end
+  end
 end
